a43feaf66dfb5af46b1ee1ca1084952bf791b795
NoonSleeper <noonsleeper@protonmail.com>
2020-12-13 17:31:13 -0300

    :hammer_and_wrench: syntax enhancement for codium.sh

--- codium.sh
+++ codium.sh
@@ -9,9 +9,9 @@ function msg() {
   echo "flatpak-codium: $*" >&2
 }
 
-if [ ! -f ${FIRST_RUN} ]; then
+if [ ! -f "${FIRST_RUN}" ]; then
   WARNING_FILE="/app/share/codium/README.md"
-  touch ${FIRST_RUN}
+  touch "${FIRST_RUN}"
 fi
 
 PYTHON_SITEDIR=$(python3 <<EOFPYTHON
@@ -22,43 +22,44 @@ EOFPYTHON
 )
 
 for tool_dir in /app/tools/*; do
-  tool_bindir=$tool_dir/bin
-  if [ -d "$tool_bindir" ]; then
-    msg "Adding $tool_bindir to PATH"
-    export PATH=$PATH:$tool_bindir
+  tool_bindir=${tool_dir}/bin
+  if [ -d "${tool_bindir}" ]; then
+    msg "Adding ${tool_bindir} to PATH"
+    export PATH=${PATH}:${tool_bindir}
   fi
-  tool_pythondir=$tool_dir/$PYTHON_SITEDIR
-  if [ -d "$tool_pythondir" ]; then
-    msg "Adding $tool_pythondir to PYTHONPATH"
-    if [ -z "$PYTHONPATH" ]; then
-      export PYTHONPATH=$tool_pythondir
+  tool_pythondir=${tool_dir}/$PYTHON_SITEDIR
+  if [ -d "${tool_pythondir}" ]; then
+    msg "Adding ${tool_pythondir} to PYTHONPATH"
+    if [ -z "${PYTHONPATH}" ]; then
+      export PYTHONPATH=${tool_pythondir}
     else
-      export PYTHONPATH=$PYTHONPATH:$tool_pythondir
+      export PYTHONPATH=${PYTHONPATH}:${tool_pythondir}
     fi
   fi
 done
 
-if [ "$FLATPAK_ENABLE_SDK_EXT" = "*" ]; then
+if [ "${FLATPAK_ENABLE_SDK_EXT}" = "*" ]; then
   SDK=()
   for d in /usr/lib/sdk/*; do
     SDK+=("${d##*/}")
   done
 else
-  IFS=',' read -ra SDK <<< "$FLATPAK_ENABLE_SDK_EXT"
+  IFS=',' read -ra SDK <<< "${FLATPAK_ENABLE_SDK_EXT}"
 fi
 
 for i in "${SDK[@]}"; do
-  if [[ -d /usr/lib/sdk/$i ]]; then
-    msg "Enabling SDK extension \"$i\""
-    if [[ -f /usr/lib/sdk/$i/enable.sh ]]; then
-      . /usr/lib/sdk/$i/enable.sh
+  if [[ -d /usr/lib/sdk/${i} ]]; then
+    msg "Enabling SDK extension \"${i}\""
+    if [[ -f /usr/lib/sdk/${i}/enable.sh ]]; then
+      # shellcheck source=/dev/null
+      . "/usr/lib/sdk/${i}/enable.sh"
     else
-      export PATH=$PATH:/usr/lib/sdk/$i/bin
+      export PATH=${PATH}:/usr/lib/sdk/${i}/bin
     fi
   else
-    msg "Requested SDK extension \"$i\" is not installed"
+    msg "Requested SDK extension \"${i}\" is not installed"
   fi
 done
 
 exec env PATH="${PATH}:${XDG_DATA_HOME}/node_modules/bin" \
-  /app/share/codium/bin/codium --extensions-dir=${XDG_DATA_HOME}/codium/extensions "$@" ${WARNING_FILE}
+  /app/share/codium/bin/codium --extensions-dir="${XDG_DATA_HOME}/codium/extensions" "${@}" "${WARNING_FILE}"
